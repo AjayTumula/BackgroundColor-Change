986d0b6a9be7fa1f50592b038bffd27475c7a5c9
AjayTumula <ajaytumula72@gmail.com>
2024-02-24 12:51:35 +0000

    made changes to the color directory and implemented the Counter app

---
+++ Counter App/app.js
@@ -0,0 +1,30 @@
+// set initial value
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('increase')){
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        if(value > 0) {
+            value.style.color = "green";
+        }
+        if(value < 0) {
+            value.style.color = "red";
+        }
+        if(value = 0) {
+            value.style.color ="#222";
+        }
+        value.textContent = count;
+    })
+});
